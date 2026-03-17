40f7e0c13d176d06a3f4b72109a0e878e9490f10
Justhossa <hossatzn@gmail.com>
2026-03-17 18:26:27 +0100

    forumreg

---
+++ app/components/RegisterForum.tsx
@@ -0,0 +1,8 @@
+export default function RegisterForm() {
+  return (
+    <div style={{ color: "white" }}>
+      <h2>Register</h2>
+      <p>Qui apparirà il form di registrazione.</p>
+    </div>
+  );
+}
